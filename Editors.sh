b8731cab8056a2fc1466e31e2edc6ebd9a80e48a
Jon Highley <jdhighley02@gmail.com>
2025-03-05 13:06:51 -0500

    Create Editors.sh

---
+++ Editors.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+pacman -S nano neovim python-pynvim ipython gunicorn python-colorama python-pygments kitty alacritty geany geany-plugins meld --needed --noconfirm &&
+
+echo "This Script is now Complete!"
