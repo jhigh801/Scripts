9e995b7082a774d096b251402183c2c6e06532b5
Jon Highley <jdhighley02@gmail.com>
2025-03-05 13:17:27 -0500

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pacman -S networkmanager network-manager-applet nm-connection-editor iw iwd dhcpcd bluez bluez-libs bluez-utils bluez-tools reflector rsync wpa_supplicant wireless_tools wireless-regdb python-pip python-pipx python-setuptools python-lxml python-lxml-docs python-pysocks python-lockfile python-html5libs python-lxml-html-clean python-genshi python-beautifulsoup4 python-cssselect tk tcl firewalld ufw gufw geoip geoip-database geoip-database-extra geoipupdate gd ed xorg-twm xterm --needed --noconfirm &&
+pacman -S networkmanager network-manager-applet nm-connection-editor iw iwd dhcpcd openresolv bluez bluez-libs bluez-utils bluez-tools reflector rsync wpa_supplicant wireless_tools wireless-regdb python-pip python-pipx python-setuptools python-lxml python-lxml-docs python-pysocks python-lockfile python-html5libs python-lxml-html-clean python-genshi python-beautifulsoup4 python-cssselect tk tcl firewalld ufw gufw geoip geoip-database geoip-database-extra geoipupdate gd ed xorg-twm xterm --needed --noconfirm &&
 
 systemctl enable NetworkManager.service &&
 systemctl enable bluetooth.service &&
